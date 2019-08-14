b23485bd3d2ef5f88b289fee71aab5033e0c2865
James Welch <117206+jamesewelch@users.noreply.github.com>
2019-08-14 09:28:21 -0400

    Initial Commit

---
+++ CutPageSpreads/Program.cs
@@ -0,0 +1,106 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.IO; 
+
+// modified from
+// https://stackoverflow.com/questions/27011829/divide-one-page-pdf-file-in-two-pages-pdf-file
+
+namespace CutPageSpreads
+{
+    class Program
+    {
+        private static string sourceFile = string.Empty;
+        private static string destFile = string.Empty;
+        private static bool execute = true;
+
+        static void Main(string[] args)
+        {
+            // proc args
+            ProcessArguments(args);
+            
+            if(execute == false)
+            {
+                PrintHelp();
+                return;
+            }
+
+            // Creating a reader
+            PdfReader reader = new PdfReader(sourceFile);
+            int pageCount = reader.NumberOfPages;
+
+            // step 1
+            Rectangle mediabox = new Rectangle(GetHalfPageSize(reader.GetPageSizeWithRotation(1)));
+            Document document = new Document(mediabox);
+
+            // step 2
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(destFile, FileMode.Create));
+
+            // step 3
+            document.Open();
+
+            // step 4
+            PdfContentByte content = writer.DirectContent;
+            PdfImportedPage page;
+
+            for (int i = 1; i <= pageCount; i++)
+            {
+                page = writer.GetImportedPage(reader, i);
+                content.AddTemplate(page, 0, 0);
+                document.NewPage();
+                content.AddTemplate(page, -mediabox.Width, 0);
+                mediabox = new Rectangle(GetHalfPageSize(reader.GetPageSizeWithRotation(i)));
+                document.SetPageSize(mediabox);
+                document.NewPage();
+            }
+
+            // step 5
+            document.Close();
+            reader.Close();
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine("Instructions:");
+            Console.WriteLine("CutPageSpread.exe [source.pdf] [output.pdf]"); 
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+
+        private static void ProcessArguments(string[] args)
+        {
+            execute = true;
+
+            sourceFile = args[0];
+
+            if (File.Exists(sourceFile) == false)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("ERROR ");
+                Console.WriteLine("The source file should exist!");
+                Console.WriteLine("");
+                sourceFile = string.Empty;
+                execute = false;
+            }
+
+            if (args.Length > 1)
+            {
+                destFile = args[1];
+            }
+
+            if (string.IsNullOrEmpty(destFile))
+            {
+                destFile = "cut_" + Path.GetFileName(sourceFile);
+            }
+        }
+
+        private static Rectangle GetHalfPageSize(Rectangle pagesize)
+        {
+            float width = pagesize.Width;
+            float height = pagesize.Height;
+            return new Rectangle(width / 2, height);
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# pdf-cut-spreads
-Executable that cuts page spreads in half, so a 17x11 = 2 pages of 8.5 x 11
+# About
+
+This is a simple executable that cuts page spreads in half. So a PDF with a page size of 17x11 will result in 2 PDF pages of 8.5 x 11.
+
+## Instructions
+
+
+`CutPageSpreads.exe <filename1.pdf>`
+
+The first argument is the input file name. The default output file will be named "cut_<filename1>.pdf".
+
+`CutPageSpreads.exe <filename1.pdf> <filename2.pdf>`
+
+The first argument is the input file name. The second argument is the output file name.
+
+# Drag and Drop
+
+If you drag and drop a PDF file on the executable, it will pass in the filename as the input file and create the default output file of "cut_<filename>.pdf".
+
